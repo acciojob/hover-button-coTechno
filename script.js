d095762ad201d8eb3afe811a2646bc53c964d2d5
Zehra Khan <khan77040307@gmail.com>
2022-12-03 20:44:14 +0530

    .

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your code here
+const btn = document.getElementById('btn');
+const notHover = () =>{
+    btn.style.backgroundColor= '#000';
+    btn.style.color= '#fff';
+    btn.style.borderRadius= '5px';
+    btn.style.border= 'none';
+}
+const hover = () =>{
+    btn.style.backgroundColor= 'yellow';
+    btn.style.color= '#000';
+    btn.style.borderRadius= '30px';
+}
